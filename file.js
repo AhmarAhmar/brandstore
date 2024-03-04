e6a0fbc68c6f600a879227a664dea4c3479cabd2
AhmarAhmar <142708003+AhmarAhmar@users.noreply.github.com>
2024-03-04 00:26:50 -0800

    brandstore

---
+++ file.js
@@ -0,0 +1,71 @@
+// console.log("AHMAR");
+
+
+
+
+let minRange = document.querySelector(".min_range")
+let min_inp=document.querySelector("min_inp");
+
+let maxRange = document.querySelector(".max_range")
+let max_inp = document.querySelector("#max_inp");
+
+
+minRange.addEventListener("input", () => {
+    // console.log(minRange.value)
+    min_input.innerHTML=minRange.value;
+    // console.log(min_inp);
+})
+
+maxRange.addEventListener("input", ()=> {
+ max_input.innerHTML=maxRange.value;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let minRange = document.querySelector(".min-range")
+// let min_inp=document.querySelector("#min_inp");
+
+// let maxRange = document.querySelector(".max-range")
+// let max_inp = document.querySelector("#max_inp");
+
+
+// minRange.addEventListener("input", () => {
+//     // console.log(minRange.value)
+//     min_inp.value=minRange.value;
+//     // console.log(min_inp);
+
+
+
+// })
+
+
+// maxRange.addEventListener("input", ()=> {
+
+//     max_inp.value=maxRange.value;
+// })
